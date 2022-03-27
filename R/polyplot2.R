193d5ef244784ed753226e1acc3af3f8e34a83ff
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-03-27 16:22:46 -0500

    Updated images

--- R/polyplot2.R
+++ R/polyplot2.R
@@ -5,19 +5,21 @@ polyplot2 <- function(x,histogram=FALSE,curve=FALSE,main="PolyPlot2",xlab=NULL,x
 # Determine plot limits
   dens <- density(x);
   if (is.null(xlim)) {xlim=c(floor(min(dens$x)),ceiling(max(dens$x)))};
-  maxdens <- max(dens$y);
-  y1 <- maxdens/4; y2 <- y1*2; y3 <- y1*3; y4 <- y1*4;
+  if (histogram) {a <- invisible(hist(x,plot=FALSE)); maxdens <- max(a$counts)}
+  else {maxdens <- max(dens$y)};
   if (is.null(ylim)) {ylim <- c(0,maxdens*1.1)};
 # Create basic and background plots
   par(mar=c(5,5,4,5))
   if(is.null(xlab)) {xlab <- substitute(x)};
   if (!histogram) {histborder="white"; histbg="white"} else {histborder=bg; histbg=tcol(bg,10)};
-  hist1 <- hist(x,prob=TRUE,plot=TRUE,axes=FALSE,col=histbg,border=histborder,xlab=xlab,ylab=NULL,xlim=xlim,ylim=ylim,main=main);
-  if (curve) {polygon(dens,col=tcol(bg,10),border=bg)}
-  axis(1,at=hist1$mids); if (histogram | curve) {axis(2); mtext("Density",side=2,line=3)};
+  if (histogram) {prob=FALSE} else {prob=TRUE}
+  hist1 <- hist(x,prob=prob,plot=TRUE,axes=FALSE,col=histbg,border=histborder,xlab=xlab,ylab=NULL,xlim=xlim,ylim=ylim,main=main);
+  axis(1,at=hist1$mids); 
+  if (histogram) {axis(2); mtext("Frequency",side=2,line=3)};
+  if (curve) {axis(2); mtext("Density",side=2,line=3); polygon(dens,col=tcol(bg,10),border=bg)};
 # Calculate polyplot statistics
   mode <- hist1$mids[which.max(hist1$counts)]
-  ymode <- hist1$density[which.max(hist1$counts)]
+  if (histogram) {ymode <- max(hist1$counts)} else {ymode <- hist1$density[which.max(hist1$counts)]};
   y1 <- ymode/4; y2 <- y1*2; y3 <- y1*3; y4 <- y1*4;
   n <- length(x); n2 <- floor(n/2); if(n%%2==0) (n2m1 <- n2+1) else (n2m1 <- n2+2);
   sx <- sort(x); n2m1 <- n2+1; first <- sx[1:n2]; second <- sx[n2m1:n];
@@ -28,7 +30,7 @@ polyplot2 <- function(x,histogram=FALSE,curve=FALSE,main="PolyPlot2",xlab=NULL,x
   # fence1 <- q1-(1.5*iqr); fence2 <- q3+(1.5*iqr);
   # points(c(fence1,fence2),c(0,0),pch=124,col=col,cex=cex)
 # Plot poly points and segments
-  points(c(mi,(mi+ma)/2,ma),c(0,0,0),pch=15,col=col,cex=cex)
+  points(c(mi,(mi+ma)/2,ma),c(0,0,0),pch=15,col=col,cex=cex);
   segments(mi,0,ma,0,lwd=2,col=col);
   points(xmean,y1,pch=19,col=col,cex=cex);
   if (fskew>=0) {segments(xmean,y1,xmean+s,y1,lwd=2,col=col)} else {segments(xmean,y1,xmean+s,y1,lty='dashed',col=col)};
