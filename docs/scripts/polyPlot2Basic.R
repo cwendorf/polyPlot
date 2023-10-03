658800407171fd8e795bc8b84286a76933ac07c1
Craig Wendorf <cwendorf@uwsp.edu>
2023-10-03 05:27:52 -0500

    Updated vignettes

---
+++ docs/scripts/polyPlot2Basic.R
@@ -0,0 +1,16 @@
+# polyPlot2
+## Basic Tutorial for PolyPlot2
+
+### Package Loading
+
+source("http://raw.githubusercontent.com/cwendorf/polyPlot2/main/source-polyPlot2.R")
+
+### The PolyPlot
+
+Scores <- round(rnorm(100,10,2),0)
+polyPlot(Scores)
+
+### Adding Background Plots
+
+polyPlot(Scores,type="frequency")
+polyPlot(Scores,type="density")
