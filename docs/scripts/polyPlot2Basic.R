ae0bbd30aff71a9eaeea45efd6b66828a8c89092
Craig Wendorf <cwendorf@uwsp.edu>
2023-10-29 05:10:34 -0500

    Restyled code

--- docs/scripts/polyPlot2Basic.R
+++ docs/scripts/polyPlot2Basic.R
@@ -7,10 +7,10 @@ source("http://raw.githubusercontent.com/cwendorf/polyPlot2/main/source-polyPlot
 
 ### The PolyPlot
 
-Scores <- round(rnorm(100,10,2),0)
+Scores <- round(rnorm(100, 10, 2), 0)
 polyPlot(Scores)
 
 ### Adding Background Plots
 
-polyPlot(Scores,type="frequency")
-polyPlot(Scores,type="density")
+polyPlot(Scores, type = "frequency")
+polyPlot(Scores, type = "density")
--- docs/scripts/polyPlot2CoverImage.R
+++ docs/scripts/polyPlot2CoverImage.R
@@ -7,5 +7,5 @@ source("http://raw.githubusercontent.com/cwendorf/polyPlot2/main/source-polyPlot
 
 ### The Basic PolyPlot
 
-Scores <- round(rnorm(100,10,2),0)
-polyPlot(Scores,type="frequency",bg="darkgoldenrod")
+Scores <- round(rnorm(100, 10, 2), 0)
+polyPlot(Scores, type = "frequency", bg = "darkgoldenrod")
